a2b850358c5c24d742a140e1729e366ca58a0b7b
LencyR <lencyrivera216912@gmail.com>
2024-10-18 15:53:23 +0800

    first commit

--- app/page.js
+++ app/page.js
@@ -1,100 +1,97 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <header className="shadow-md p-4 fixed w-full top-0 left-0 z-10 bg-white">
+        <ul>
+          <div className="flex justify-between">
+            <div className="flex space-x-2 items-center">
+              <li><h1 className="font-bold text-xl">nuxplay</h1></li>
+              <img
+                src="https://yt3.googleusercontent.com/ytc/AIdro_k7RWEIXhS1uWBcJ9omrQIF5M-CM12rLkw6gL62R1I545M=s900-c-k-c0x00ffffff-no-rj"
+                alt="nixplay-logo"
+                className="w-[5vh] h-[5vh]" />
+            </div>
+            <div className="flex space-x-2 items-center">
+              <li>
+                <Image
+                  src="/images/united.png"
+                  alt="Description of the image"
+                  width={30}
+                  height={30}
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/uk.png"
+                  alt="Description of the image"
+                  width={30}
+                  height={30}
+                />
+              </li>
+              <li>
+                <Image
+                  src="/images/german.png"
+                  alt="Description of the image"
+                  width={30}
+                  height={30}
+                />
+              </li>
+            </div>
+          </div>
+        </ul>
+      </header>
+      <main>
+        <div className="snap-y snap-mandatory scroll-smooth h-screen">
+          <section className="snap-start h-screen flex justify-center items-center">
+            <div className="space-y-10">
+              <div className="flex flex-col content-center space-y-2">
+                <div className="flex justify-center items-center">
+                  <h1 className="font-[900] text-2xl">nuxplay</h1>
+                  <img
+                    src="https://yt3.googleusercontent.com/ytc/AIdro_k7RWEIXhS1uWBcJ9omrQIF5M-CM12rLkw6gL62R1I545M=s900-c-k-c0x00ffffff-no-rj"
+                    alt="nixplay-logo"
+                    className="w-[5vh] h-[5vh]" />
+                </div>
+                <div>
+                  <p className="text-2xl text-gray-500">Wi-FI Cloud Frames</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex flex-col content-center space-y-2">
+                  <h4 className="text-center">Connect With</h4>
+                  <div className="flex">
+                    <p>amazon alexa</p>
+                    <p>flickr</p>
+                    <p>Google Photos</p>
+                  </div>
+                  <div className="flex">
+                    <p>instagram</p>
+                    <p>facebook</p>
+                    <p>Dropbox</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="snap-start h-screen flex justify-center items-center">
+            <div className="mt-10">
+              <img src="https://m.media-amazon.com/images/I/71hXZfsv2zL.jpg" alt="" className="w-[45vh] h-[45vh]" />
+            </div>
+            <div>
+              <h1>NIXPLAY EDGE</h1>
+              <p>Basic and black with high-resolution display</p>
+            </div>
+            <div>
+              <button>Learn More</button>
+            </div>
+            <div>
+              <h1>8"</h1>
+            </div>
+          </section>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
       </footer>
     </div>
   );
